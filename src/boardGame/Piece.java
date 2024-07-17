b5207d16264ac8c2881566732bd7c4f7a1742be2
Ziery-Dev <zieryDev@gmail.com>
2024-07-17 15:58:18 -0300

    Possiveis movimentos das peças implementado

--- src/boardGame/Piece.java
+++ src/boardGame/Piece.java
@@ -1,6 +1,6 @@
 package boardGame;
 
-public class Piece {
+public abstract class Piece {
 	protected Position position; 
 	private Board board;
 	
@@ -16,9 +16,27 @@ public class Piece {
 	protected Board getBoard() { //para não ser acessado por camadas externas
 		return board;
 	}
-
-
 	
+	//possiveis movimentos da peça
+	public abstract boolean [][] possibleMoves();
+	
+	//recebe uma posição e retorna se é possivel ou não mover-se para aquele local
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+	
+	//informa se existe pelo menos um mivimento possivel para determinada peça
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] mat = possibleMoves();
+		for (int i = 0; i < mat.length; i++) {
+			for(int j =0; j < mat.length; j++) {
+				if(mat[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 	
 	
 	
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -47,6 +47,9 @@ public class ChessMatch {
 		if(!board.thereIsApiece(position)) {
 			throw new ChessException("Não existe peça na posição de origem");
 		}
+		if(!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException ("Não existe movimentos possiveis para a peça selecionada");
+		}
 	}
 	
 	
